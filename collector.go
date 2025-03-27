b66d3fd619cce34b30301f71b54a77d9ad1c4c61
rweb22 <nocomplexno@gmail.com>
2025-03-27 00:51:26 -0700

    Fixed Timestamp

--- collector.go
+++ collector.go
@@ -136,7 +136,7 @@ func fetchStockData(pdDatas map[string]interface{}, metacacheData map[int]map[st
 
 		if _, ok := data["data"]; ok {
 			if dataMap, ok := data["data"].(map[string]interface{}); ok {
-				t, err := time.Parse(time.RFC3339, dataMap["last_updated_at"].(string))
+				t, err := time.Parse(time.RFC3339, dataMap["last_updated_at"].In(loc).(string))
 				if err != nil {
 					fmt.Println("Error parsing time:", err)
 				} else {
